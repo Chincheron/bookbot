f8e54307e6115d754709742d8c8509d00cf976d6
Chincheron <jmhyde891@gmail.com>
2025-01-29 21:30:04 -0500

    L10 - add function to count each character

--- main.py
+++ main.py
@@ -8,9 +8,24 @@ def word_count(string):
     words = string.split()
     #print(words)
     print(len(words))
-    return len(words)
+
+def character_count(string):
+    #convert to lower case for comparison
+    lower_string = string.lower()
+   
+    #iterate through each character and assign to dictionary   
+    character_counts = {}
+    for i in lower_string:
+        if i in character_counts:
+            character_counts[i] = character_counts[i] + 1
+        else:
+            character_counts[i] = 1
+    print(character_counts)
+
 
 text = main()
 
 word_count(text)
+
+character_count(text)
 
